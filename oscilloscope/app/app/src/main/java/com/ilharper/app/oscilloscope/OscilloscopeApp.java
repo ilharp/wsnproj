abb6a540fd3f0ff16bbd6cb0bae31935e5e1ef00
Il Harper <afanyiyu@hotmail.com>
2021-12-27 15:24:09 +0800

    Init OscilloscopeApp

---
+++ oscilloscope/app/app/src/main/java/com/ilharper/app/oscilloscope/OscilloscopeApp.java
@@ -0,0 +1,8 @@
+package com.ilharper.app.oscilloscope;
+
+import android.app.Application;
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class OscilloscopeApp extends Application {
+}
