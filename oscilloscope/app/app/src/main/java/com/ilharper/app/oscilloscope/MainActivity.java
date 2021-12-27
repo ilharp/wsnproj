6eeee7197aa705aee4798b557862c1d7814dc9c7
Il Harper <afanyiyu@hotmail.com>
2021-12-28 02:17:07 +0800

    Add AndroidEntryPoint

--- oscilloscope/app/app/src/main/java/com/ilharper/app/oscilloscope/MainActivity.java
+++ oscilloscope/app/app/src/main/java/com/ilharper/app/oscilloscope/MainActivity.java
@@ -2,7 +2,9 @@ package com.ilharper.app.oscilloscope;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import dagger.hilt.android.AndroidEntryPoint;
 
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
     @Override
